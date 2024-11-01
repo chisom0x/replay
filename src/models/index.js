d7b163804347b151b7f0ca50293df03cb7977967
chisomOx <onyenankiekelvin@gmail.com>
2024-11-01 22:59:37 +0100

    modified:   src/models/index.js

--- src/models/index.js
+++ src/models/index.js
@@ -6,7 +6,7 @@ import { Sequelize } from 'sequelize';
 import process from 'process';
 import dotenv from 'dotenv';
 
-// Load environment variables from .env file
+// Load environment variables
 dotenv.config();
 
 const basename = path.basename(__filename);
@@ -17,18 +17,27 @@ const config = await import(`${path.resolve()}/config/config.json`, {
   assert: { type: 'json' }
 }).then(module => module.default)[env];
 
+// Set database configuration based on the environment
+if (env === 'development') {
+  config.username = process.env.DB_DEV_USERNAME || config.username;
+  config.password = process.env.DB_DEV_PASSWORD || config.password;
+  config.database = process.env.DB_DEV_DATABASE || config.database;
+  config.host = process.env.DB_DEV_HOST || config.host;
+} else if (env === 'production') {
+  config.username = process.env.DB_PROD_USERNAME || config.username;
+  config.password = process.env.DB_PROD_PASSWORD || config.password;
+  config.database = process.env.DB_PROD_DATABASE || config.database;
+  config.host = process.env.DB_PROD_HOST || config.host;
+}
+
 const db = {};
 
 let sequelize;
 
-// Determine the password based on the environment
-const password =
-  env === 'production' ? process.env.DB_PROD_PASSWORD : process.env.DB_DEV_PASSWORD;
-
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, password, {
+  sequelize = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
     dialect: config.dialect,
     port: config.port,
@@ -36,8 +45,7 @@ if (config.use_env_variable) {
   });
 }
 
-fs
-  .readdirSync(__dirname)
+fs.readdirSync(__dirname)
   .filter(file => {
     return (
       file.indexOf('.') !== 0 &&
