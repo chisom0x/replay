77219f8ae3c81b26409fea34b8dcfe96d339f106
chisomOx <onyenankiekelvin@gmail.com>
2024-11-19 23:56:02 +0100

    modified:   app.js 	modified:   package-lock.json 	modified:   package.json

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 import cookieParser from 'cookie-parser';
+import hpp from 'hpp'
 import router from './src/routes/index.js';
 import globalErrorHandler from './src/utils/global_error_handler.js';
 
@@ -12,6 +15,12 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100,
+  message: 'Too many requests, please try again later.',
+});
+
 export const createServer = () => {
   const app = express();
 
@@ -19,13 +28,18 @@ export const createServer = () => {
   app.options('*', cors(corsOptions));
   app.use(express.json());
   app.use(cookieParser());
+  app.use(helmet());
+  app.use(hpp())
+  app.use('/api', limiter)
+
+  app.use(globalErrorHandler);
+
+
+  app.use('/api/v1/replay', router);
 
   app.get('/', (req, res) => {
     res.json({ message: 'Hello World' });
   });
-
-  app.use('/api/v1/replay', router);
-  app.use(globalErrorHandler);
 
   return app;
 };
